f60bcfc4db526f7f9875386e75c01ddde82ccaba
chloepasc <chloepasc@yahoo.com>
2022-10-26 12:38:24 -0500

    first update

---
+++ index.js
@@ -0,0 +1,25 @@
+var open = false;
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    if(open === false){
+        document.getElementById("the-navbar").style.width = "200px";
+        document.getElementById("main").style.marginLeft = "75px";
+        document.getElementById("poop").style.marginLeft = "125px";
+        // document.getElementById("main").style.marginRight = "50px";
+        document.getElementById("poop").style.transform = "rotate(180deg)";
+        open = true;
+    }else{
+        closeNav();
+        open = false;
+    }
+
+
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("the-navbar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("poop").style.marginLeft = "0";
+    document.getElementById("poop").style.transform = "rotate(0deg)" 
+  }
